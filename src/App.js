3410c5b5f85580c09ff72c77bc190896b96a8020
Poovannan98 <poovannan098@gmail.com>
2023-02-02 13:21:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import "./App.css";
-import Login from "./Components/Authentication/Login";
-// import background from "./images/elearning.jpg";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <>
-      {/* <div
-        style={{
-          backgroundImage: `url(${background})`,
-          height: "100vh",
-          marginTop: "-35px",
-          // fontSize: "50px",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <h1>Poo Class Event Management for Students</h1>
-      </div> */}
-      <Login/>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
